9e3c981e2ba26685f4a744ff8c71cfacf14cb3fe
Tyler Beaver <tyler9725@gmail.com>
2018-08-07 22:26:18 -0500

    new way of doing things here. 8)

--- sample.py
+++ sample.py
@@ -1,19 +1,108 @@
-import frlg
-from scripting import script_section
+from scripting import Build, encode_text as apply_LUT
+from functools import partial
 
-@script_section
-def sample_text_display(text_location):
-	yield frlg.lock()
-	yield frlg.faceplayer()
-	yield frlg.preparemsg(text_location)
-	yield frlg.closeonkeypress()
-	yield frlg.release()
-	yield frlg.end()
+encode_text = partial(apply_LUT, {
+	"ß":b"\x15",
+	"é":b"\x1B",
+	"0":b"\xA1",
+	"1":b"\xA2",
+	"2":b"\xA3",
+	"3":b"\xA4",
+	"4":b"\xA5",
+	"5":b"\xA6",
+	"6":b"\xA7",
+	"7":b"\xA8",
+	"8":b"\xA9",
+	"9":b"\xAA",
+	"!":b"\xAB",
+	"-":b"\xAE",
+	"«":b"\xB1",
+	"»":b"\xB2",
+	"<":b"\xB3",
+	"'":b"\xB4",
+	"$":b"\xB7",
+	",":b"\xB8",
+	"*":b"\xB9",
+	"/":b"\xBA",
+	"A":b"\xBB",
+	"B":b"\xBC",
+	"C":b"\xBD",
+	"D":b"\xBE",
+	"E":b"\xBF",
+	"F":b"\xC0",
+	"G":b"\xC1",
+	"H":b"\xC2",
+	"I":b"\xC3",
+	"J":b"\xC4",
+	"K":b"\xC5",
+	"L":b"\xC6",
+	"M":b"\xC7",
+	"N":b"\xC8",
+	"O":b"\xC9",
+	"P":b"\xCA",
+	"Q":b"\xCB",
+	"R":b"\xCC",
+	"S":b"\xCD",
+	"T":b"\xCE",
+	"U":b"\xCF",
+	"V":b"\xD0",
+	"W":b"\xD1",
+	"X":b"\xD2",
+	"Y":b"\xD3",
+	"Z":b"\xD4",
+	"a":b"\xD5",
+	"b":b"\xD6",
+	"c":b"\xD7",
+	"d":b"\xD8",
+	"e":b"\xD9",
+	"f":b"\xDA",
+	"g":b"\xDB",
+	"h":b"\xDC",
+	"i":b"\xDD",
+	"j":b"\xDE",
+	"k":b"\xDF",
+	"l":b"\xE0",
+	"m":b"\xE1",
+	"n":b"\xE2",
+	"o":b"\xE3",
+	"p":b"\xE4",
+	"q":b"\xE5",
+	"r":b"\xE6",
+	"s":b"\xE7",
+	"t":b"\xE8",
+	"u":b"\xE9",
+	"v":b"\xEA",
+	"w":b"\xEB",
+	"x":b"\xEC",
+	"y":b"\xED",
+	"z":b"\xEE",
+	":":b"\xF0",
+	"Ä":b"\xF1",
+	"Ö":b"\xF2",
+	"Ü":b"\xF3",
+	" ":b"\x00",
+	"\n":b"\xFE"
+})
 
-if __name__=="__main__":
-	import sys
-	# I don't actually know of any text locations off the top of my head lol
-	TEXT_LOCATION = 0x08123456
-	if len(sys.argv) > 1:
-		TEXT_LOCATION = int(sys.argv[1])
-	sys.stdout.buffer.write(sample_text_display(TEXT_LOCATION))
+def message(script_engine):
+	yield from map(encode_text,[
+		"Hello, world!"
+	])
+	yield b'\xFF'
+
+def speak(script_engine):
+	yield from script_engine.preparemsg(message)
+	yield from script_engine.waitmsg()
+	yield from script_engine.closeonkeypress()
+	yield from script_engine._return()
+
+def entry(script_engine):
+	yield from script_engine.lock()
+	yield from script_engine.call(speak)
+	yield from script_engine.release()
+	yield from script_engine.end()
+
+with Build.build(__name__) as compile:
+	compile(message)
+	compile(speak)
+	compile(entry)
--- scripting.py
+++ scripting.py
@@ -1,57 +1,184 @@
-# the goal of this project is to create a library
-# that can be used to generate script (bytecode)
-# files in a consistent manner that takes advantage
-# of python's higher level language features.
-# think of it as a compiler with a python pre-processor.
-# kind of.
-import struct
-import functools
+from struct import pack, calcsize
+from functools import partial
+from sys import stdout
+from json import load
+from io import TextIOWrapper
+import argparse
 
-def __simple_return(*a): return a
+class Compiler:
+	REF_FMT = '<I'
+	def __init__(self,engine):
+		self.engine = engine
+		self.compiled = {} # func_name: bytes
+		self.references = {} # func_name: [ (position, reference)]
+	def compile(self, f, *a, **k):
+		funcname = f.__name__
+		self.compiled[funcname] = b''
+		for chunk in f(self.engine,*a,**k):
+			if isinstance(chunk,bytes):
+				self.compiled[funcname] = self.compiled[funcname] + chunk
+			elif callable(chunk):
+				pos = len(self.compiled[funcname])
+				self.compiled[funcname] = self.compiled[funcname] + pack(self.REF_FMT,0)
+				ref = chunk.__name__
+				self.references[funcname] = self.references.get(funcname,[])
+				self.references[funcname].append((pos,ref))
+		return f
 
-def script_func(structure,b):
-	comp = functools.partial(struct.pack,structure)
-	def wrap(f):
-		def replace(*a,**k):
-			return comp(b,*f(*a,**k))
-		replace.total_size = struct.calcsize(structure)
-		replace.__doc__ = f.__doc__
-		return replace
-	return wrap
-
-def script_section(f):
-	def wrap(func):
-		def replace(*a,**k):
-			r = b''
-			for chunk in func(*a,**k):
-				if chunk is None:
+class Knower:
+	def __init__(self, src):
+		self.data = src
+	def find_space(self,size,used=None,byte=0xFF,start=0x000000, align=0x02):
+		used = (used or [])[:]
+		cursor, scan = start, start
+		data = self.data
+		dlen = len(data)
+		print(dlen)
+		while (scan - cursor) < size and scan < dlen:
+			if len(used):
+				if scan in used[0]:
+					cursor = scan = used[0].stop + 1
+					used = used[1:]
 					continue
-				r = r + chunk
-			return r
-		replace.__doc__ = func.__doc__
-		return replace
-	return wrap(f)
+			if data[scan]!=byte:
+				scan += 1
+				while scan % align != 0:
+					scan += 1
+				cursor = scan
+			else:
+				scan += 1
+		if scan - cursor >= size:
+			return cursor
+		raise Exception("Could not find {} bytes = {} aligned to {} after position 0x{:06X} of 0x{:06X} (cursor: 0x{:06X})".format(size, byte, align, start, dlen, cursor))
 
-def no_args(v,help_string=""):
-	def func(): return (v,)
-	func.__doc__ = help_string
-	return script_func('',v)(func)
+ 
+class Linker:
+	def __init__(self,knower, compiler):
+		self.knower = knower
+		self.compiler = compiler
+	def replace_references(self, data, references, positions):
+		ref_fmt = self.compiler.REF_FMT
+		ref_sz = calcsize(ref_fmt)
+		for (position, ref) in references:
+			data_L = data[:position]
+			ref = positions[ref] | 0x08000000
+			data_ref = pack(ref_fmt, ref)
+			data_R = data[position+ref_sz:]
+			data = data_L + data_ref + data_R
+		return data
+	def compile(self, freespace_starts = 0x000000, freespace_align = 0x02, freespace_byte=0xFF):
+		compiled = self.compiler.compiled
+		sections = compiled.items()
+		positions = {}
+		used = []
+		for func_name, bytecode in sections:
+			sz = len(bytecode)
+			pos = positions[func_name] = self.knower.find_space(sz, used,start=freespace_starts, align=freespace_align, byte=freespace_byte)
+			used.append(range(pos,pos+sz))
+		return {
+			func_name: {
+				"data": self.replace_references(bytecode, self.compiler.references.get(func_name,[]), positions),
+				"position": positions[func_name]
+			}
+			for func_name, bytecode in sections
+		}
 
-def direct_arguments(structure,b,help_string=""):
-	def x(*a):return a
-	x.__doc__ = help_string
-	return script_func(structure,b)(x)
-
-def install_commands(cls,cmd_list):
-	for cmd in cmd_list:
-		m = direct_arguments(cmd['structure'],cmd['cmd'],help_string=cmd['doc_string'])
-		name = cmd['name']
-		m.__name__ = name
-		setattr(cls,name,m)
+class PatchMaker:
+	def __init__(self, data, bulk = None, truncate = None):
+		self.data = data
+		self.bulk_records = bulk or []
+		self._truncate = truncate
+	@classmethod
+	def make_addr(cls,addr):
+		return pack(">I",addr)[1:]
+	@property
+	def truncate(self):
+		return self.make_addr(self._truncate) if self._truncate else b''
+	def compile(self):
+		return b'PATCH' + b''.join([
+			self.make_addr(record['position']) + pack('>H',len(record['data'])) + record['data']
+			for record in self.data
+		]) + b''.join([
+			self.make_addr(record['position']) + b'\x00\x00' + pack(">HB",record['count'],record['value'])
+			for record in self.bulk_records
+		]) + b'EOF' + self.truncate
 
 class ScriptEngine:
 	def __init__(self):
 		pass
+	def install(self,cmd_list):
+		def script_func(cmd,*args):
+			args = [cmd['cmd']] + list(args)
+			fmt = cmd["structure"]
+			order = fmt[0] if fmt[0] in "@<>=!" else '<'
+			structure = fmt[1:] if fmt[0] in "@<>=!" else fmt[:]
+			for segment, val in zip(structure,args):
+				if callable(val):
+					assert segment=='I'
+					yield val
+				else:
+					segment = order + segment
+					yield pack(segment,val)
+
+		for cmd in cmd_list:
+			m = partial(script_func, cmd)
+			m.__doc__ = cmd['doc_string']
+			m.__name__ = cmd['name']
+			setattr(self,cmd['name'],m)
+
+def encode_text(LUT,text):
+	return b''.join(LUT.get(c,c) for c in text)
+
+def build_argparser():
+	parser = argparse.ArgumentParser(description='Compiles the script into an IPS patch.')
+	parser.add_argument("-target","--t",type=argparse.FileType(mode='rb'),dest='target')
+	parser.add_argument("-output","--o",type=argparse.FileType(mode='wb'),dest='output')
+	parser.add_argument("-engine","--e",type=argparse.FileType(mode='r',encoding='utf-8'),dest='engine')
+	return parser
+
+class DummyContextManager:
+	def __init__(self):
+		pass
+	def __enter__(self):
+		return self
+	def __exit__(self, etype, exception, tb):
+		pass
+
+class DummyCompileContext(DummyContextManager):
+	def __call__(self,*a,**k):
+		pass
+
+class Build:
+	def __init__(self, target, output, engine, freespace_start=0x740000):
+		data = target.read()
+		self.knower = Knower(data)
+		self.freespace_start=freespace_start
+
+		self.engine = ScriptEngine()
+		engine = load(engine)
+		self.engine.install(engine)
+
+		self.compiler = Compiler(self.engine)
+
+		self.linker = Linker(self.knower, self.compiler)
+
+		self.output = output.buffer if isinstance(output,TextIOWrapper) else output
+		
+	def __enter__(self):
+		return self.compiler.compile
+	def __exit__(self, etype, exception, tb):
+		if etype is None and exception is None and tb is None:
+			linked = self.linker.compile(freespace_starts=self.freespace_start)
+			pm = PatchMaker(linked.values())
+			self.output.write(pm.compile())
+			if self.output is not stdout.buffer:
+				for func_name, record in linked:
+					print("{: 20}: inserted at 0x{:06X} is {} bytes long.".format(func_name, record['position'], len(record['data'])))
 	@classmethod
-	def install(cls,cmd_list):
-		install_commands(cls,cmd_list)
+	def build(cls,name):
+		if name=="__main__":
+			argparser = build_argparser()
+			args = argparser.parse_args()
+			return cls(args.target, args.output, args.engine)
+		else:
+			return DummyCompileContext()
